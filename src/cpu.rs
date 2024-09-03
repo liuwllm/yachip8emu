ecf67cc5b45e5b37f431236793250fcebd3eb3b8
liuwllm <liu.wllm@gmail.com>
2024-09-03 19:01:41 -0400

    Add instructions before 9XY0

--- src/cpu.rs
+++ src/cpu.rs
@@ -34,7 +34,8 @@ pub struct Emu {
     stack: Vec<u16>,
     d_timer: u8,
     s_timer: u8,
-    v_reg: [u8; NUM_V]
+    v_reg: [u8; NUM_V],
+    cosmac: bool,
 }
 
 impl Emu {
@@ -48,6 +49,7 @@ impl Emu {
             d_timer: 0,
             s_timer: 0,
             v_reg: [0; NUM_V]
+            cosmac: true
         };
 
         emu_inst.mem[..FONTSET_SIZE].copy_from_slice(&FONTSET);
@@ -65,6 +67,7 @@ impl Emu {
         self.s_timer = 0;
         self.v_reg = [0; NUM_V];
         self.mem[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+        self.cosmac = false;
     }
 
     pub fn tick(&mut self) {
@@ -106,15 +109,78 @@ impl Emu {
             // 00E0: Clear screen
             (0, 0, 0xE, 0) => {
                 self.display = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
-                print!("CLS\n");
+                println!("CLS");
+            },
+
+            // 00EE: Return from subroutine
+            (0, 0, 0xE, 0xE) => {
+                let addr = self.stack.pop();
+
+                match addr {
+                    Some(x) => {
+                        self.pc = x;
+                        println!("RET")
+                    },
+                    None => {
+                        panic!("The stack was unexpectedly empty.")
+                    }
+                }
             },
 
             // 1NNN: Jump 
             (1, _, _, _) => {
                 let nnn = op & 0x0FFF;
+
                 self.pc = nnn;
-                print!("JP {}\n", nnn);
+                
+                println!("JP {}", nnn);
             },
+
+            // 2NNN: Call subroutine
+            (2, _, _, _) => {
+                let nnn = op & 0x0FFF;
+
+                self.stack.push(nnn);
+                self.pc = nnn;
+
+                println!("CALL {}", nnn);
+            },
+
+            // 3XNN: Skip if VX = NN
+            (3, _, _, _) => {
+                let x = nibble2 as usize;
+                let nn = op & 0x00FF;
+
+                if self.v_reg[x] == nn {
+                    self.pc += 2;
+                }
+
+                println!("SE V{}, {}", x, nn);
+            },
+
+            // 4XNN: Skip if VX != NN
+            (4, _, _, _) => {
+                let x = nibble2 as usize;
+                let nn = op & 0x00FF;
+
+                if self.v_reg[x] != nn {
+                    self.pc +=2;
+                }
+
+                println!("SNE V{}. {}", x, nn);
+            }
+
+            // 5XY0: Skip if VX = VY
+            (5, _, _, 0) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                if self.v_reg[x] == self.v_reg[y] {
+                    self.pc += 2;
+                }
+
+                println!("SE V{}, V{}", x, y);
+            }
 
             // 6XNN: Set
             (6, _, _, _) => {
@@ -122,22 +188,162 @@ impl Emu {
                 let nn = (op & 0x00FF) as u8;
 
                 self.v_reg[x] = nn;
-                print!("LD V{}, {}\n", x, nn);
+
+                println!("LD V{}, {}", x, nn);
             },
             
             // 7XNN: Add
             (7, _, _, _) => {
                 let x = nibble2 as usize;
                 let nn = (op & 0x00FF) as u8;
+
                 self.v_reg[x] += nn;
-                print!("ADD V{}, {}\n", x, nn);
+
+                println!("ADD V{}, {}", x, nn);
+            },
+
+            // 8XY0: Set
+            (8, _, _, 0) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                self.v_reg[x] = self.v_reg[y];
+
+                println!("LD V{}, V{}", x, y);
+            } ,
+
+            // 8XY1: Binary OR
+            (8, _, _, 1) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                self.v_reg[x] |= self.v_reg[y];
+
+                println!("OR V{}, V{}", x, y);
+            },
+
+            // 8XY2: Binary AND
+            (8, _, _ 2) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                self.v_reg[x] &= self.v_reg[y];
+
+                println!("AND V{}, V{}", x, y);
+            },
+
+            // 8XY3: Binary XOR
+            (8, _, _, 3) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                self.v_reg[x] ^= self.v_reg[y];
+
+                println!("XOR V{}, V{}", x, y);
+            },
+
+            // 8XY4: Add with carry
+            (8, _, _, 4) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                let (sum, overflow) = self.v_reg[x].overflowing_add(self.v_reg[y]);
+                
+                if overflow {
+                    self.v_reg[0xF] = 1;
+                }
+                else {
+                    self.v_reg[0xF] = 0;
+                }
+
+                self.v_reg[x] = sum;
+
+                println!("ADD V{}, V{}", x, y);
+            },
+
+            // 8XY5: Subtract VY from VX
+            (8, _, _, 5) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                let (diff, underflow) = self.v_reg[x].overflowing_sub(self.v_reg[y]);
+
+                if underflow {
+                    self.v_reg[0xF] = 0;
+                }
+                else {
+                    self.v_reg[0xF] = 1;
+                }
+
+                self.v_reg[x] = diff;
+
+                println!("SUB V{}, V{}", x, y);
+            },
+
+            // 8XY6: Shift to right
+            (8, _, _, 6) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                if cosmac { 
+                    self.v_reg[x] = self.v_reg[y];
+                }
+                
+                let lsb = self.v_reg[x] & 1;
+                self.v_reg[x] >>= 1;
+                self.v_reg[0xF] = lsb;
+
+                println!("SHR V{}, V{}", x, y);
+            },
+
+            // 8XY7: Subtract VX from VY
+            (8, _, _, 7) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                let (diff, underflow) = self.v_reg[y].overflowing_sub(self.v_reg[x]);
+
+                if underflow {
+                    self.v_reg[0xF] = 0;
+                }
+                else {
+                    self.v_reg[0xF] = 1;
+                }
+
+                self.v_reg[x] = diff;
+                println!("SUBN V{}, V{}", x, y);
+            }
+
+            // 8XYE: Shift to left
+            (8, _, _, 0xE) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                if cosmac {
+                    self.v_reg[x] = self.v_reg[y];
+                }
+
+                let msb = (self.v_reg[x] >> 7) & 1;
+                self.v_reg[x] <<= 1;
+                self.v_reg[0xF] = msb;
+            }
+
+            // 9XY0: Skip if VX != VY
+            (9, _, _, 0) => {
+                let x = nibble2 as usize;
+                let y = nibble3 as usize;
+
+                if self.v_reg[x] != self.v_reg[y] {
+                    self.pc += 2;
+                }
+                println!("SNE V{}, V{}", x, y);
             }
 
             // ANNN: Set index
             (0xA, _, _, _) => {
                 let nnn = op & 0x0FFF;
                 self.i_reg = nnn;
-                print!("LD I, {}\n", nnn);
+                println!("LD I, {}", nnn);
             },
 
             // DXYN: Display
@@ -175,7 +381,7 @@ impl Emu {
                     self.v_reg[0xF] = 0;
                 }
 
-                print!("DRW V{}, V{}, {}\n", nibble2, nibble3, nibble4);
+                println!("DRW V{}, V{}, {}", nibble2, nibble3, nibble4);
             },
 
             (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", op),
--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,8 @@ fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() != 2 {
         println!("Usage: cargo run path/to/game");
+        println!("Options:");
+        println!("   -c: toggle original COSMAC VIP functionality");
         return;
     }
 
@@ -47,6 +49,11 @@ fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     let mut chip8_inst = Emu::new();
+
+    if &args[2] == "-c" {
+        chip8_inst.cosmac = true;
+    }
+
     let mut rom = File::open(&args[1]).expect("Unable to open file");
     let mut buffer = Vec::new();
     rom.read_to_end(&mut buffer).unwrap();
